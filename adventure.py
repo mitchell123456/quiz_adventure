97762024a8ec72eb1bd15802ceaa7c4f7a9a42e7
mitchell armstrong <marmstrong18@VenerableBede.schools.sunderland.gov.uk>
2016-11-25 10:00:19 +0000

    my adventure

--- adventure.py
+++ adventure.py
@@ -1,8 +1,126 @@
 # An adventure game
+health = 1
+isaac_matthews_health = 10
+
+
+    
+
+
+
 
 def room1():
+    global health
 
-    ... make a game
+    print("Its election day")
+    print("Youre in the booth looking at the candidates")
+    print("Donald Trump     Hilary Clinton")
+    print("But you dont see thoose names... you see")
+    print("A. Isaac Matthews   Mr Herbane")
+    print("B. Isaac Matthews  Scarce")
+    print("C. Isaac Matthews   Keemstar")
+    answer = input("Answer: ")
+
+    if answer.lower() == "a":
+        print("Do you even know who they are")
+        room2()
+
+    if answer.lower() == "b":
+        print("You just picked this because of scarce didnt you")
+        room2()
+
+    if answer.lower() == "c":
+        print("You are beyond help now")
+        room2()
+
+    print("Ok its time to cast your vote")
+    print("But wait, whats that?")
+    print("Its Isaac Matthews!!!")
+    print("A. Walk up to him and take a selfie")
+    print("B. Ignore him. And ask him why he is here")
+    print("C. Charge at him, tackle him through a table and ask who is your real dad")
+    answer = input("Answer: ")
+
+    if answer.lower() == "a":
+        print("You just want to brag and myspace")
+        room2()
+
+    if answer.lower() == "b":
+        print("You secritly want to punch him in his massive ears")
+        room2()
+
+    if answer.lower() == "c":
+        print("Firstly good choice and secondly we all know his real dad is Glen")
+        room2()
+
+
+def room2():
+
+    print("Isaac is annoyed because he heard someone talking about him having pictures of someone on his duck USB stick")
+    print("He starts to attack everyone in sight")
+    print("He is staring right at you")
+    print("A. Be a man and fight him")
+    print("B. Be an Isaac and hide under a table")
+    answer = input("Answer: ")
+
+    if answer.lower() == "a":
+        print("Enter arena. Round 1 Fight!!")
+        room3()
+
+    if answer.lower() == "b":
+        print("Coward, you are going to fight him anyway. Round 1 Fight!!")
+        room3()
+
+
+def room3():
+
+    print("Attack or talk about his dads")
+    if answer.lower() == "attack":
+        print("You deal 2 damage")
+
+    if answer.lower() == "talk about his dads":
+        print("CRITICAL HIT, 5 damage")
+
+    print("Attack or talk about his dads")
+    if answer.lower() == "attack":
+        print("CRITICAL HIT, 5 damage")
+
+    if answer.lower() == "talk about his dads":
+        print("You deal 2 damage")
+
+    print("He is weak")
+    print("Attack or talk about his dads")
+    if answer.lower() == "attack":
+        print("Well done he is dead. You should be proud!")
+        room4()
+
+    if answer.lower() == "talk about his dads":
+        print("Well done he is dead. You should be proud!")
+        room4()
+
+
+def room4():
+
+    print("You have finally killed Isaac Matthews.")
+    print("The world is saved")
+    print("BOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOM")
+    print("Oh no")
+    print("Its Donald Trump")
+    print("And hes not happy")
+    print("He says he wants to nuke cincinnati")
+    print("                                   ")
+    print("                                   ")
+    print("He nuke cincinnati and kills everyone including a gorilla called harambe")
+    print("Conspiricy theorists go nuts")
+    print("Game over")
+    
+        
+    
+        
+          
+    
+    
+    
+    
 
 
 
--- quiz.py
+++ quiz.py
@@ -1,8 +1,41 @@
 # Our quiz!
 
 def quiz():
+    name = input("Name: ")
+    score = 0
 
-    ... make a quiz!
+    print("Hello", name, "which english footballer is nominated for the 2016 Ballon d'or")
+    answer = input("Answer: ")
+
+    if answer.lower() in "jamie vardy":
+        print("Correct!")
+
+    print("Next question", name, "who is sunderland afc's all time top goal scorer")
+    answer = input("Answer: ")
+
+    if answer.lower() in "kevin phillips":
+        print("Correct!")
+
+    print("Lets continue", name, "in what year did sunderland afc win the FA cup")
+    answer = input("Answer: ")
+
+    if answer.lower() in "1973":
+        print("Correct!")
+
+    print("Next question", name, "how much did sunderland afc buy jermain defoe for")
+    answer = input("Answer: ")
+
+    if answer.lower() in "£14 million":
+        print("Correct!")
+
+    print("Final question", name, "what is sunderlands highest finish in the premier league")
+    answer = input("Answer: ")
+
+    if answer.lower() in "7":
+        print("Correct")
+        
+    
+
 
 
 
